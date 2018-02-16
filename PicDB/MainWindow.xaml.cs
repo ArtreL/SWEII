5491390d80318e7d526eeb17afcd7267924db5ea
Lukas Global <lukas.schiermayer@gmail.com>
2018-02-16 17:52:56 +0100

    Started graphical user interface.

--- PicDB/MainWindow.xaml.cs
+++ PicDB/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PicDB.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace PicDB
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
--- PicDB/ViewModels/MainWindowViewModel.cs
+++ PicDB/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using BIF.SWE2.Interfaces.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace PicDB.ViewModels
@@ -9,9 +11,16 @@ namespace PicDB.ViewModels
     public class MainWindowViewModel : IMainWindowViewModel
     {
         public IPictureViewModel CurrentPicture => new PictureViewModel();
-
         public IPictureListViewModel List => new PictureListViewModel();
-
         public ISearchViewModel Search => new SearchViewModel();
+
+        public string SearchIcon
+        {
+            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Icons\\search.png"; }
+        }
+        public string ImageDisplay
+        {
+            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Pictures\\Img1.jpg"; }
+        }
     }
 }
